b283c627ccc621d64980851925639aed34aa8fc4
Diego Lima <30624298+DiegoL1M4@users.noreply.github.com>
2019-07-22 10:22:50 -0300

    Arquivos primários

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>  </title>
+    <link rel="icon" href="favicon.ico">
+
+    <link href="../lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/index.css" rel="stylesheet">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="../lib/bootstrap/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    
+    <!-- ------------------- Conteúdo ------------------- -->
+    <div class="container">
+        <div class="col-sm-4"></div>
+        <div class="col-sm-4">
+            <div class="caixa">
+                <h2 class="titulo">Sistema de Restrição de Horários do Moodle</h2> <br>
+
+                <form method="post" action="../models/login/login.php">
+                    <label>Login:</label>
+                    <input class="form-control" type="text" name="login">
+                    <br>
+                    <label>Senha:</label>
+                    <input class="form-control" type="password" name="senha">
+
+                    <br><br>
+                    <button class="botao_login btn btn-primary" type="submit">Login</button>
+                </form>
+                
+            </div>
+        </div>
+        <div class="col-sm-4"></div>
+    </div>
+    
+    <!-- ------------------- Rodapé ------------------- -->
+    <footer class="rodape">
+        <div class="container">
+                <div class="row">
+                    <h5></h5>
+                </div>
+        </div>
+    </footer>
+
+</body>
+
+</html>
---
+++ views/home/home.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>  </title>
+	<link rel="icon" href="favicon.ico">
+
+	<link href="../../lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link href="../css/style.css" rel="stylesheet">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="../../lib/bootstrap/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+	<!-- ------------------- Barra de Navegação ------------------- -->
+	<nav class="navbar navbar-default navbar-fixed-top barra">
+		<div class="container">
+        	<div class="navbar-header">
+              	<button type="button" class="navbar-toggle collapsed"
+                      	data-toggle="collapse" data-target="#barra-navegacao">
+                    <span class="sr-only">Alternar Menu</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+              	</button>
+                <a href="index.php" class="navbar-brand">
+                <span class="logo">IFCE</span></a>
+        	</div>
+
+        	<div class="collapse navbar-collapse" id="barra-navegacao">
+                <ul class="nav navbar-nav navbar-right">
+                    <li> <a href="index.php">INÍCIO</a> </li>
+                </ul>
+        	</div>   
+        </div>
+	</nav>
+	
+<!-- ------------------- Conteúdo ------------------- -->
+    <div class="container">
+
+    </div>
+    
+<!-- ------------------- Rodapé ------------------- -->
+    <footer class="rodape">
+		<div class="container">
+		 		<div class="row">
+			 	    <h5></h5>
+		 		</div>
+		</div>
+    </footer>
+
+</body>
+
+</html>
