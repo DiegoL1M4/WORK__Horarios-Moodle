2349384c101e4da9d628f3d51343a2cbca59baa0
Diego Lima <diego-dlp@hotmail.com>
2019-08-03 18:35:52 -0300

    Função de cadastro de horários

---
+++ views/horarios/index.php
@@ -0,0 +1,16 @@
+
+<?php
+    require_once('../../views/layout/header.php');
+?>
+
+<link href="../css/home.css" rel="stylesheet">
+    
+<!-- ------------------- Conteúdo/HOME ------------------- -->
+    <br><br><br><br><br><br>
+
+    Cadastrar horários
+
+    
+<?php
+    require_once('../../views/layout/footer.php');
+?>
